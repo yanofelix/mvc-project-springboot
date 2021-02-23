c1af7e9276ff939fc922b05acaa77ffb8b6d641c
Yan Felix <devyanfelix@gmail.com>
2021-02-23 02:04:21 -0300

    paginas html prontas

---
+++ src/test/java/br/com/mvccourse/MvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mvccourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
